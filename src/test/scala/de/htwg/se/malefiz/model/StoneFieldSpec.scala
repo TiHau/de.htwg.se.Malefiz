3bb08345daf7ae7b4faf9e778e76cd3b9e78fe42
jasafasa <f.steinhauser@hotmail.de>
2017-10-29 11:55:30 +0100

    changed tests stonefield and stone

--- src/test/scala/de/htwg/se/malefiz/model/StoneFieldSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/model/StoneFieldSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 class StoneFieldSpec extends WordSpec with Matchers {
   "A Field" when {
     "new" should {
-      val field = StoneField(1, 1, true, PlaceholderStone(field))
+      val field:StoneField = StoneField(1, 1, true, PlaceholderStone(null))
 
       "have a x coordinate" in {
         field.x should be(1)
