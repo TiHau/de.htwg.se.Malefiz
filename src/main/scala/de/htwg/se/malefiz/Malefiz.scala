1393ee5b886adb910f173149f5a4d8aa0edac479
jasafasa <f.steinhauser@hotmail.de>
2017-10-30 14:44:49 +0100

    edit of main

--- src/main/scala/de/htwg/se/malefiz/Malefiz.scala
+++ src/main/scala/de/htwg/se/malefiz/Malefiz.scala
@@ -4,7 +4,10 @@ import de.htwg.se.malefiz.model.{InitializeMalefiz, GameBoard}
 
 object Malefiz {
   def main(args: Array[String]): Unit = {
-    InitializeMalefiz.apply().pri(InitializeMalefiz.apply().setPlayerStones(InitializeMalefiz.apply().setBlockStones(InitializeMalefiz.apply().buildMalefitzGameBoard(GameBoard.board))))
+    InitializeMalefiz.apply().pri(InitializeMalefiz.apply()
+      .setPlayerStones(InitializeMalefiz.apply()
+        .setBlockStones(InitializeMalefiz.apply()
+         .buildMalefitzGameBoard(GameBoard.board))))
 
   }
 }
