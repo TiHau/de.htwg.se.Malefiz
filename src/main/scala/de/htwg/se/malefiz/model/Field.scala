6081cdfcf15bd1d7c7f533db7e10522efa5b7154
HuntedHunter <tim.hauenstein97@gmx.de>
2017-11-03 09:20:10 +0100

    removed isEmpty parameter

--- src/main/scala/de/htwg/se/malefiz/model/Field.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Field.scala
@@ -1,15 +1,15 @@
 package de.htwg.se.malefiz.model
 
-abstract class Field(x:Int, y:Int, isEmpty:Boolean, isFieldType:Char){
+abstract class Field(x:Int, y:Int, isFieldType:Char){
   def getFieldType(): Char ={
     isFieldType
   }
 }
 
-case class EmptyField (x:Int, y:Int ,isEmpty:Boolean) extends Field(x, y, isEmpty, isFieldType = 'e') {
+case class EmptyField (x:Int, y:Int) extends Field(x, y, isFieldType = 'e') {
 
 }
 
-case class StoneField (x:Int,y:Int ,isEmpty:Boolean, stone:Stone) extends Field(x, y, isEmpty, isFieldType = 's'){
+case class StoneField (x:Int, y:Int, stone:Stone) extends Field(x, y, isFieldType = 's'){
 
 }
