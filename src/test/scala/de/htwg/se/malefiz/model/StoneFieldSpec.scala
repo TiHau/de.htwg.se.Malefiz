b80aac7ae2587c711dd452fee23a4d7ec3e8f0b4
jasafasa <f.steinhauser@hotmail.de>
2017-11-03 09:43:13 +0100

    changed tests

--- src/test/scala/de/htwg/se/malefiz/model/StoneFieldSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/model/StoneFieldSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 class StoneFieldSpec extends WordSpec with Matchers {
   "A Field" when {
     "new" should {
-      val field:StoneField = StoneField(1, 1, true, FreeStone(null))
+      val field:StoneField = StoneField(1, 1, FreeStone(null))
 
       "have a x coordinate" in {
         field.x should be(1)
--- src/test/scala/de/htwg/se/malefiz/model/StoneSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/model/StoneSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class StoneSpec extends WordSpec with Matchers {
   "A Stone" when { "new" should {
-    val stone:PlayerStone = PlayerStone(StoneField(1,1,false, null),Player(1))
+    val stone:PlayerStone = PlayerStone(StoneField(1,1, null),Player(1))
     "have a Player"  in {
       stone.player should be(Player(1))
     }
