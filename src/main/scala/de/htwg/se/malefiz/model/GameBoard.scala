e55e781f6fb2f045c549c45d24282cae2c50d514
HuntedHunter <tim.hauenstein97@gmx.de>
2017-11-03 12:17:08 +0100

    optimzed Field and Stone class

--- src/main/scala/de/htwg/se/malefiz/model/GameBoard.scala
+++ src/main/scala/de/htwg/se/malefiz/model/GameBoard.scala
@@ -1,4 +1,4 @@
 package de.htwg.se.malefiz.model
 object GameBoard {
-  var board = Array.ofDim[Field]((InitializeMalefiz.apply().nu16) + 1,InitializeMalefiz.apply().nu16)
+  var board = Array.ofDim[AbstractField]((InitializeMalefiz.apply().nu16) + 1,InitializeMalefiz.apply().nu16)
 }
