74f2e8e03cafefde044ddba6009becf859bc3f63
jasafasa <f.steinhauser@hotmail.de>
2017-11-03 09:34:09 +0100

    delete of stonefield, emptyfield datei

--- src/main/scala/de/htwg/se/malefiz/model/EmptyField.scala
+++
@@ -1,5 +0,0 @@
-package de.htwg.se.malefiz.model
-
-case class EmptyField (x:Int, y:Int) extends Field(x, y, isFieldType = 'e') {
-
-}
--- src/main/scala/de/htwg/se/malefiz/model/StoneField.scala
+++
@@ -1,5 +0,0 @@
-package de.htwg.se.malefiz.model
-
-case class StoneField (x:Int,y:Int ,stone:Stone) extends Field(x, y, isFieldType = 's'){
-
-}
