4dd9a6bd05b0f472ad57ddace0561fb36c2f023b
HuntedHunter <tim.hauenstein97@gmx.de>
2017-12-19 16:07:12 +0100

    added method markPossibleMovesOfStone some Observer stuff

---
+++ src/main/scala/de/htwg/se/malefiz/controller/Observer.scala
@@ -0,0 +1,12 @@
+package de.htwg.se.malefiz.controller
+
+trait Observer {
+  def update
+}
+
+class Observable {
+  var subscribers: Vector[Observer] = Vector()
+  def add(s: Observer) = subscribers = subscribers :+ s
+  def remove(s: Observer) = subscribers = subscribers.filterNot(o => o == s)
+  def notifyObservers = subscribers.foreach(o => o.update)
+}
--- src/main/scala/de/htwg/se/malefiz/model/Player.scala
+++ src/main/scala/de/htwg/se/malefiz/model/Player.scala
@@ -2,11 +2,6 @@ package de.htwg.se.malefiz.model
 
 case class Player(color: Int){
   private val numberOfStones = 5
-  private val six = 6
-  def dice(): Int ={
-    val diced = scala.util.Random.nextInt(six) + 1
-    diced
-  }
 
   val stones = Array.ofDim[PlayerStone](numberOfStones)
 }
