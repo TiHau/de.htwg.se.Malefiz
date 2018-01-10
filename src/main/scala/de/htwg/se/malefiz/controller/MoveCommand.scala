fe602398d41b0e0289889c6c4ff662139853aa95
HuntedHunter <tim.hauenstein97@gmx.de>
2018-01-10 15:29:08 +0100

    changed Fielname MoveCommand

--- src/main/scala/de/htwg/se/malefiz/controller/MoveCommand.scala
+++ src/main/scala/de/htwg/se/malefiz/controller/MoveCommand.scala
@@ -5,14 +5,14 @@ import de.htwg.se.malefiz.model.{Field, PlayerStone}
 
 class MoveCommand(stone: PlayerStone, destField: Field, controller: Controller) extends Command {
 
-  val xStone = stone.actualField.asInstanceOf[Field].x
-  val yStone = stone.actualField.asInstanceOf[Field].y
-  val currentField = controller.gameBoard.board(xStone)(yStone).asInstanceOf[Field]
+  private val xStone = stone.actualField.asInstanceOf[Field].x
+  private val yStone = stone.actualField.asInstanceOf[Field].y
+  private val currentField = controller.gameBoard.board(xStone)(yStone).asInstanceOf[Field]
 
   override def doStep: Unit =   controller.gameBoard.changeTwoStones(currentField, destField)
 
   override def undoStep: Unit = controller.gameBoard.changeTwoStones(destField, currentField)
 
   override def redoStep: Unit = controller.gameBoard.changeTwoStones(currentField, destField)
-  
+
 }
