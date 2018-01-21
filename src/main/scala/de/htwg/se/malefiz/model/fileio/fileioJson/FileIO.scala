a78841ad3324876f9a286ea1015e57f349e2538a
jasafasa <f.steinhauser@hotmail.de>
2018-01-21 17:20:46 +0100

    file io dosent write to list

--- src/main/scala/de/htwg/se/malefiz/model/fileio/fileioJson/FileIO.scala
+++ src/main/scala/de/htwg/se/malefiz/model/fileio/fileioJson/FileIO.scala
@@ -5,6 +5,8 @@ import de.htwg.se.malefiz.model.fileio.FileIOInterface
 import de.htwg.se.malefiz.model.gameboard.{Field, PlayerStone}
 import play.api.libs.json._
 
+import scala.collection.immutable.TreeMap
+
 class FileIO extends FileIOInterface{
   //override def load: Option[ControllerInterface] = ???
 
@@ -16,53 +18,53 @@ class FileIO extends FileIOInterface{
   }
 
   def gameBoardToJson(controller: ControllerInterface): JsObject = {
-    var jsObject = new JsObject()
-    var jsObjectBoard = new JsObject()
-    var jsObjectController = new JsObject()
+    var jsObject= new JsObject(new TreeMap())
+    var jsObjectBoard =new JsObject(new TreeMap())
+    var jsObjectController =new JsObject(new TreeMap())
     val playerCount = controller.gameBoard.playerCount
-    jsObjectBoard.++(Json.obj("playerCount"->JsNumber(playerCount)))
+    jsObjectBoard.+( "playerCount"->JsNumber(playerCount))
     for (y <- 0 to 15) {
       for (x <- 0 to 16) {
         val abstractField = controller.gameBoard.board(x)(y)
         val isfreeSpace = abstractField.isFreeSpace()
-        jsObjectBoard.++(Json.obj("isFreeSpace"->JsBoolean(isfreeSpace)))
+        jsObjectBoard.+("isFreeSpace"->JsBoolean(isfreeSpace))
         if (!isfreeSpace) {
           val field = abstractField.asInstanceOf[Field]
           val avariable=field.avariable
-          jsObjectBoard.++(Json.obj("avariable"->JsBoolean(avariable)))
+          jsObjectBoard.+("avariable"->JsBoolean(avariable))
           val x = field.x
-          jsObjectBoard.++(Json.obj("x"->JsNumber(x)))
+          jsObjectBoard.+("x"->JsNumber(x))
           val y =field.y
-          jsObjectBoard.++(Json.obj("y"->JsNumber(y)))
+          jsObjectBoard.+("y"->JsNumber(y))
           val stone = field.stone
           val sort = stone.sort
-          jsObjectBoard.++(Json.obj("sort"->JsString(sort.toString)))
+          jsObjectBoard.fields +"sort"->JsString(sort.toString)
           if (stone.sort == 'p') {
             val playerStone = stone.asInstanceOf[PlayerStone]
             val startFieldX = playerStone.startField.asInstanceOf[Field].x
-            jsObjectBoard.++(Json.obj("startX"->JsNumber(startFieldX)))
+            jsObjectBoard.fields +"startX"->JsNumber(startFieldX)
             val startFieldY = playerStone.startField.asInstanceOf[Field].y
-            jsObjectBoard.++(Json.obj("startY"->JsNumber(startFieldY)))
+            jsObjectBoard.fields +"startY"->JsNumber(startFieldY)
             val playerColor = playerStone.playerColor
-            jsObjectBoard.++(Json.obj("playerColor"->JsNumber(playerColor)))
+            jsObjectBoard.fields +"playerColor"->JsNumber(playerColor)
           }
         }
       }
     }
 
     val activeColor  =controller.activePlayer.color
-    jsObjectController.++(Json.obj("activeColor"->JsNumber(activeColor)))
+    jsObjectController.fields +"activeColor"->JsNumber(activeColor)
     val diced = controller.diced
-    jsObjectController.++(Json.obj("diced"->JsNumber(diced)))
+    jsObjectController.fields +"diced"->JsNumber(diced)
     val state = controller.state
-    jsObjectController.++(Json.obj("state"->JsString(state.toString)))
+    jsObjectController.fields +"state"->JsString(state.toString)
     val needToSetBlockStone = controller.needToSetBlockStone
-    jsObjectController.++(Json.obj("needToSetBlockStone"->JsBoolean(needToSetBlockStone)))
+    jsObjectController.fields +"needToSetBlockStone"->JsBoolean(needToSetBlockStone)
     val commandNotExecuted = controller.commandNotExecuted
-    jsObjectController.++(Json.obj("commandNotExecuted"->JsBoolean(commandNotExecuted)))
+    jsObjectController.fields +"commandNotExecuted"->JsBoolean(commandNotExecuted)
 
-    jsObject.++(jsObjectBoard)
-    jsObject.++(jsObjectController)
+    jsObject.+ ("gameBoard" -> jsObjectBoard)
+    jsObject.+ ("controller"-> jsObjectController)
     jsObject
 
   }
