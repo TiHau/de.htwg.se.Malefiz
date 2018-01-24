caeb37ce960069107ebfffc0d75e279e02f1b3c0
jasafasa <f.steinhauser@hotmail.de>
2018-01-24 14:37:04 +0100

    added new tests with other players on turn and non valid file

--- src/test/scala/de/htwg/se/malefiz/model/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/malefiz/model/FileIOSpec.scala
@@ -16,7 +16,6 @@ class FileIOSpec extends WordSpec with Matchers {
   val injector: Injector = Guice.createInjector(new MalefizModule)
   val fileIO: FileIOInterface = injector.instance[FileIOInterface]
   var controller: ControllerInterface = injector.getInstance(classOf[ControllerInterface])
-  controller.setPlayerCount(2)
   "A FileIO" when {
     "gameStart" should {
       if (Files.exists(Paths.get("saveFile.json"))) {
@@ -27,7 +26,8 @@ class FileIOSpec extends WordSpec with Matchers {
         Files.exists(Paths.get("saveFile.json")) && Files.exists(Paths.get("saveFile.xml")) shouldBe false
       }
 
-      "should can save and load then file exists and restore count" in {
+      "should can save and load then file exists and restore count with player 3" in {
+        controller.setPlayerCount(2)
         fileIO.save(controller)
         controller.setPlayerCount(4)
         fileIO.load(controller)
@@ -35,7 +35,42 @@ class FileIOSpec extends WordSpec with Matchers {
         controller.gameBoard.playerCount shouldBe 2
 
       }
+      "should can save and load then file exists and restore count with player 2" in {
+        controller.setPlayerCount(3)
+        fileIO.save(controller)
+        controller.setPlayerCount(4)
+        fileIO.load(controller)
+        Files.exists(Paths.get("saveFile.json")) || Files.exists(Paths.get("saveFile.xml")) shouldBe true
+        controller.gameBoard.playerCount shouldBe 3
 
+      }
+      "should can save and load then file exists and restore count with player 1" in {
+        controller.setPlayerCount(2)
+        fileIO.save(controller)
+        controller.setPlayerCount(4)
+        fileIO.load(controller)
+        Files.exists(Paths.get("saveFile.json")) || Files.exists(Paths.get("saveFile.xml")) shouldBe true
+        controller.gameBoard.playerCount shouldBe 2
+
+      }
+      "should can save and load then file exists and restore count with player 4" in {
+        controller.setPlayerCount(3)
+        fileIO.save(controller)
+        controller.setPlayerCount(4)
+        fileIO.load(controller)
+        Files.exists(Paths.get("saveFile.json")) || Files.exists(Paths.get("saveFile.xml")) shouldBe true
+        controller.gameBoard.playerCount shouldBe 3
+
+      }
+      "non existing file" in {
+
+        if (Files.exists(Paths.get("saveFile.json"))) {
+          Files.delete(Paths.get("saveFile.json"))
+        } else if (Files.exists(Paths.get("saveFile.xml"))) { Files.delete(Paths.get("saveFile.xml")) }
+        controller.setPlayerCount(4)
+        fileIO.load(controller)
+        controller.gameBoard.playerCount shouldBe 4
+      }
     }
   }
 }
